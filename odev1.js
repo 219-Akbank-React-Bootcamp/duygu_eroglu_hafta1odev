b92f75fb40688288a9cb67e41ade745a25c2ce75
Duygu Eroğlu <45760467+duygueroglu@users.noreply.github.com>
2022-09-04 14:31:42 +0300

    odev1.js updated

--- odev1.js
+++ odev1.js
@@ -7,9 +7,16 @@
 //İpucu 1: Array.prototype.includesCi =  dedikten sonra metodunuzu yazabilirsiniz
 //İpucu 2: "HaKaN".toUppercase() size "HAKAN" sonucunu üretir
 
+Array.prototype.includesCi = function(search) {
+    if(this.find((val)=> val.toLowerCase()===search.toLowerCase())) 
+      return true
+    else 
+      return false;
+  
 //Test :
 
-const array = ["Patika","219","Akbank","React","Bootcamp"]
+const dizi = ["Patika","219","Akbank","React","Bootcamp"]
 
-console.log(array.includesCi("patika")===true ? "Beklendiği gibi" : "Beklendiği gibi değil")
-console.log(array.includesCi("kırmızı")===false ? "Beklendiği gibi" : "Beklendiği gibi değil")
+console.log(dizi.includesCi("patika")===true ? "Beklendiği gibi" : "Beklendiği gibi değil")
+console.log(dizi.includesCi("kırmızı")===true ? "Beklendiği gibi" : "Beklendiği gibi değil")
+console.log(dizi.includesCi("akbaNK")===true ? "Beklendiği gibi" : "Beklendiği gibi değil")
